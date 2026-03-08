bc4b7d4fd22655862adff7b5fdd5140a465ecd8c
Aziz Chharchhodawala <172964858+aziz-ch52@users.noreply.github.com>
2026-03-08 19:54:40 +0530

    Add Option interface for CRUD operations

---
+++ src/controllers/Option.java
@@ -0,0 +1,17 @@
+package controllers;
+
+// Interface for different operations in the system
+public interface Option {
+
+    // Method to create a new record
+    void create();
+
+    // Method to view records
+    void read();
+
+    // Method to update records
+    void update();
+
+    // Method to delete records
+    void delete();
+}
